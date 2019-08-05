db85eb6a8ec7e962b86f6b5d23137736513d0235
Ahmed Gado <a.gado.2df@gmail.com>
2019-08-05 10:26:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const Scooter = {'year', 'color', 'model' };
-const Driver = {'name', 'age', 'experience'};
-const PickUpLocation = {'adress', 'city'};
+const Scooter = {'year': 'color': 'model' };
+const Driver = {'name': 'age': 'experience'};
+const PickUpLocation = {'adress': 'city'};
